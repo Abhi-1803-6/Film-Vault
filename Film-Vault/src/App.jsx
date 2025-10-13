c8ab2f7b63c9855b944da3da2f0b8b546765b276
Abhi-1803-6 <abkr1839@gmail.com>
2025-10-14 01:22:54 +0530

    setting up routing using react router dom

--- Film-Vault/src/App.jsx
+++ Film-Vault/src/App.jsx
@@ -1,11 +1,24 @@
 import './App.css'
+import Movies from './components/Movies'
 import Navbar from './components/Navbar'
+import Watchlist from './components/Watchlist'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
- 
+
   return (
     <>
-      <Navbar/>
+      <BrowserRouter>
+
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Movies />} />
+          <Route path='/watchlist' element={<Watchlist />} />
+
+
+        </Routes>
+
+      </BrowserRouter>
     </>
   )
 }
--- Film-Vault/src/components/Navbar.jsx
+++ Film-Vault/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Logo from '../assets/movieLogo.png'
+import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div className='flex border space-x-8 items-center pl-3 pr-4'>
         <img src={Logo} className="inline h-12 w-12 mr-2" alt="Cinema Icon"/>
-        <a href='/' className='text-blue-500 text-xl font-bold'>Home</a>
-        <a href='/watchlist' className='text-blue-500 text-xl font-bold'>Watchlist</a>
+        <Link to='/' className='text-blue-500 text-xl font-bold'>Movies</Link>
+        <Link to='/watchlist' className='text-blue-500 text-xl font-bold'>Watchlist</Link>
     </div>
   )
 }
